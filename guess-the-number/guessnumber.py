278c311edc03057ae768660007ed44ab21cd51de
terramare <peter206@gmail.com>
2012-10-26 15:25:44 +0200

    function complete?
    Still need to comment

--- guess-the-number/guessnumber.py
+++ guess-the-number/guessnumber.py
@@ -8,6 +8,7 @@ import simplegui
 # initialize global variables used in your code
 num_range = 100
 secret_number = 0
+i = 7
 
 # define event handlers for control panel
 
@@ -15,15 +16,17 @@ def init():
     global num_range
     if num_range == 100:
         range100()
-        print "Please enter a number between 0 and",num_range
+        print "New Game. Enter a number between 0 and",num_range
     elif num_range == 1000:
         range1000()
-        print "Please enter a number between 0 and",num_range
+        print "New Game. Enter a number between 0 and",num_range
 
 def range100():
     # button that changes range to range [0,100) and restarts
     global secret_number
     global num_range
+    global i
+    i = 7
     num_range = 100
     secret_number = random.randrange(0, 101)
     return secret_number
@@ -32,6 +35,8 @@ def range1000():
     # button that changes range to range [0,1000) and restarts
     global secret_number
     global num_range
+    global i
+    i = 10
     num_range = 1000
     secret_number = random.randrange(0, 1001)
     return secret_number
@@ -39,26 +44,35 @@ def range1000():
 def get_input(guess):
     # main game logic goes here
     global secret_number
-    print "Please enter a number between 0 and",num_range
+    global i
+    # print "Please enter a number between 0 and",num_range
+    # if i == 0:
     print "You guessed " + guess
     print secret_number
     if int(guess) == secret_number:
-        print "Your guess is correct!"
+        print "Your guess is correct!\n"
         init()
     elif int(guess) > secret_number:
+        i = i - 1
         print "Lower"
+        print "You have", i,"guesses left. Try again..\n"
     elif int(guess) < secret_number:
+        i = i - 1
         print "Higher"
-    else:
-        print "Please enter a number between 0 and",num_range
-
+        print "You have", i,"guesses left. Try again..\n"
+    if i == 0:
+        print "You've run out of guesses. Try a new game.."
+        init()
+        
 # create frame
 frame = simplegui.create_frame("Guess the Number", 200, 200)
 
 # register event handlers for control elements
-frame.add_button("Range is [0, 100)", range100, 200)
-frame.add_button("Range is [0, 1000)", range1000, 200)
+frame.add_button("Range: 0 - 100", range100, 200)
+frame.add_button("Range: 0 - 1000", range1000, 200)
 frame.add_input("Enter a guess", get_input, 200)
 
 # start frame
 frame.start()
+
+init()
