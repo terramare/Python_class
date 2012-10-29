6b02d5cc084fc4bcc4d233facfc9fb4771f2631a
terramare <peter206@gmail.com>
2012-10-29 21:41:26 +0100

    Almost complete
    The only incomplete part, I think, is not incrementing total_stops if
    the timer is already stopped when the stop button is pressed again.

--- stopwatch/stopwatch.py
+++ stopwatch/stopwatch.py
@@ -1,49 +1,58 @@
 # template for "Stopwatch: The Game"
 
 import simplegui
-import math
 
 # define global variables
 timer_int = 0
+success_stops = 0
+total_stops = 0
 
 # define helper function format that converts integer
 # counting tenths of seconds into formatted string A:BC.D
 def format(timer_int):
-	A = timer_int
-    tenths = timer_int - math.floor(timer_int)
-    
+    A = timer_int // 600
+    B = timer_int % 600 // 100
+    C = timer_int % 100 // 10 
+    D = timer_int % 10
+    return str(A) + ":" + str(B) + str(C) + "." + str(D)
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start_handler():
     tick.start()
     
 def stop_handler():
+    global timer_int, total_stops, success_stops
     tick.stop()
+    total_stops = total_stops + 1
+    if timer_int % 10 == 0:
+        success_stops = success_stops + 1
     
 def reset_handler():
-    global timer_int
+    global timer_int, total_stops, success_stops
     tick.stop()
     timer_int = 0
+    total_stops = 0
+    success_stops = 0
 
 def draw(canvas):
-    global timer_int
-    canvas.draw_text(str(timer_int), (50, 50), 24, "White")
+    global timer_int, success_stops, total_stops
+    canvas.draw_text(format(timer_int), (130, 175), 72, "White")
+    canvas.draw_text(str(success_stops) + "/" + str(total_stops), (430, 40), 24, "Red")
 
 # define event handler for timer with 0.1 sec interval
 def timer_handler():
     global timer_int
     timer_int = timer_int + 1
-    print timer_int
 
 # create frame
-frame = simplegui.create_frame("Stopwatch Game", 300, 200)
+frame = simplegui.create_frame("Stopwatch Game", 500, 300)
 
 # register event handlers
 start = frame.add_button("Start", start_handler)
 stop = frame.add_button("Stop", stop_handler)
 reset = frame.add_button("Reset", reset_handler)
-frame.set_draw_handler(draw)
 tick = simplegui.create_timer(100, timer_handler)
+frame.set_draw_handler(draw)
 
 # start timer and frame
 frame.start()
