0e90d8cc232d6d160c85751115e9493a1cbd527c
terramare <peter206@gmail.com>
2012-11-06 15:42:43 +0100

    Everything but scoring

--- pong/pong.py
+++ pong/pong.py
@@ -41,14 +41,19 @@ def draw(c):
     global paddle1_vel, paddle2_vel
  
     # update paddle's vertical position, keep paddle on the screen
-    if paddle1_pos > HALF_PAD_HEIGHT and paddle1_pos < HEIGHT - HALF_PAD_HEIGHT:
+    if paddle1_pos >= HALF_PAD_HEIGHT and paddle1_pos <= HEIGHT - HALF_PAD_HEIGHT:
         paddle1_pos += paddle1_vel
-    else:
-        paddle1_pos -= paddle1_vel
-
-    paddle2_pos += paddle2_vel
+    elif paddle1_pos <= HALF_PAD_HEIGHT:
+        paddle1_pos = HALF_PAD_HEIGHT
+    elif paddle1_pos >= HEIGHT - HALF_PAD_HEIGHT:
+        paddle1_pos = HEIGHT - HALF_PAD_HEIGHT
+    if paddle2_pos >= HALF_PAD_HEIGHT and paddle2_pos <= HEIGHT - HALF_PAD_HEIGHT:
+        paddle2_pos += paddle2_vel
+    elif paddle2_pos <= HALF_PAD_HEIGHT:
+        paddle2_pos = HALF_PAD_HEIGHT
+    elif paddle2_pos >= HEIGHT - HALF_PAD_HEIGHT:
+        paddle2_pos = HEIGHT - HALF_PAD_HEIGHT
     
-        
     # draw mid line and gutters
     c.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
     c.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
@@ -63,9 +68,15 @@ def draw(c):
     ball_pos[1] += ball_vel[1]
     if ball_pos[1] <= BALL_RADIUS or ball_pos[1] >= (HEIGHT - BALL_RADIUS):
         ball_vel[1] = - ball_vel[1]
-    if ball_pos[0] <= PAD_WIDTH + BALL_RADIUS:
+    if ball_pos[0] - BALL_RADIUS <= PAD_WIDTH:
+        if ball_pos[1] <= paddle1_pos + HALF_PAD_HEIGHT and ball_pos[1] >= paddle1_pos - HALF_PAD_HEIGHT:
+            ball_vel[0] = -(ball_vel[0] * 1.1)
+    if ball_pos[0] + BALL_RADIUS >= WIDTH - PAD_WIDTH:
+        if ball_pos[1] <= paddle2_pos + HALF_PAD_HEIGHT and ball_pos[1] >= paddle2_pos - HALF_PAD_HEIGHT:
+            ball_vel[0] = -(ball_vel[0] * 1.1)
+    if ball_pos[0] < PAD_WIDTH:
         ball_init(True)
-    if ball_pos[0] >= WIDTH - PAD_WIDTH - BALL_RADIUS:
+    if ball_pos[0] > WIDTH - PAD_WIDTH:
         ball_init(False)
         
     # draw ball and scores
@@ -73,26 +84,26 @@ def draw(c):
     
     
 def keydown(key):
-    global paddle1_vel, paddle2_vel
+    global paddle1_vel, paddle2_vel, paddle1_pos, HALF_PAD_HEIGHT
     if key==simplegui.KEY_MAP["w"]:
-        paddle1_vel -= 4
+            paddle1_vel = -4
     elif key==simplegui.KEY_MAP["s"]:
-        paddle1_vel += 4
+            paddle1_vel = 4
     elif key==simplegui.KEY_MAP["up"]:
-        paddle2_vel -= 4
+        paddle2_vel = -4
     elif key==simplegui.KEY_MAP["down"]:
-        paddle2_vel += 4
+        paddle2_vel = 4
    
 def keyup(key):
     global paddle1_vel, paddle2_vel
     if key==simplegui.KEY_MAP["w"]:
-        paddle1_vel += 4
+        paddle1_vel = 0
     elif key==simplegui.KEY_MAP["s"]:
-        paddle1_vel -= 4
+        paddle1_vel = 0
     elif key==simplegui.KEY_MAP["up"]:
-        paddle2_vel += 4
+        paddle2_vel = 0
     elif key==simplegui.KEY_MAP["down"]:
-        paddle2_vel -= 4
+        paddle2_vel = 0
 
 # create frame
 frame = simplegui.create_frame("Pong", WIDTH, HEIGHT)
@@ -105,4 +116,3 @@ frame.add_button("Restart", init, 100)
 # start frame
 init()
 frame.start()
-
